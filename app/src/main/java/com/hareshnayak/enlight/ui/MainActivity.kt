a1d3a53006fe9dd0d2d49f5c19693eb131fe710c
hareshnayak <sketchharry01@gmail.com>
2021-12-31 21:04:17 +0530

    Fragments navigation completed

--- app/src/main/java/com/hareshnayak/enlight/ui/MainActivity.kt
+++ app/src/main/java/com/hareshnayak/enlight/ui/MainActivity.kt
@@ -2,11 +2,23 @@ package com.hareshnayak.enlight.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.hareshnayak.enlight.R
+import com.hareshnayak.enlight.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
     }
 }
